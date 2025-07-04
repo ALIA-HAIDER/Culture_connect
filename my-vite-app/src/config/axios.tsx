5b3911ccef261be665ec0077cd45f40982305a51
Alia Haider <turabhaider786@outlook.com>
2025-07-04 16:24:10 +0530

    backend deployed link updated frontend deployed link updated

--- my-vite-app/src/config/axios.tsx
+++ my-vite-app/src/config/axios.tsx
@@ -2,7 +2,7 @@ import axios,{AxiosInstance} from 'axios';
 
 
 export const axiosInstance:AxiosInstance =axios.create({
-    baseURL :" http://localhost:4500/api",
+    baseURL :" https://culture-connect.onrender.com/api",
     headers :{
         "Content-Type": "application/json",
     },
